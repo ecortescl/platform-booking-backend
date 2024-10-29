9d72235d0384eafc861e42b26a3173b004a8f1e5
Esteban Cortés R <hola@ecortes.cl>
2024-10-29 20:02:45 -0300

    role

--- migrations/3-create-permissions-role.js
+++ migrations/3-create-permissions-role.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('PermissionsRoles', {
+    await queryInterface.createTable("PermissionsRoles", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       idRole: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'roles',
-          key: 'id'
-        }
+          model: "Roles",
+          key: "id",
+        },
       },
       idPermission: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'permissions',
-          key: 'id'
-        }
-      }
+          model: "permissions",
+          key: "id",
+        },
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('PermissionsRoles');
-  }
+    await queryInterface.dropTable("PermissionsRoles");
+  },
 };
