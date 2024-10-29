646b484bec56d2e4259b47e5ad485d9594125fad
Esteban Cortés R <hola@ecortes.cl>
2024-10-29 19:20:43 -0300

    production testing

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('booking', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+const database = process.env.DB_DATABASE || "booking";
+const username = process.env.DB_USERNAME || "root";
+const password = process.env.DB_PASSWORD || "";
+const host = process.env.DB_HOST || "localhost";
+const dialect = "postgres";
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
   logging: false,
 });
 
--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,43 +1,60 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
+const { User, Role } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Método para inciar sesión
 exports.loginUser = [
-    // Validation and sanitization
-    body('email').isEmail().withMessage('Email inválido').normalizeEmail(),
-    body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres').notEmpty().withMessage('La contraseña no debe estar vacía'),
-  
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      try {
-        const user = await User.findOne({
-          where: { email: req.body.email },
-          include: [{
-            model: Role,
-            attributes: ['name']
-          }]
-        });
-  
-        if (!user) return res.status(400).send('Email o contraseña incorrectos.');
-  
-        const validPass = await bcrypt.compare(req.body.password, user.password);
-  
-        if (!validPass) return res.status(400).send('Email o contraseña incorrectos.');
-  
-        const token = jwt.sign({ id: user.id, role: user.Role.name }, process.env.JWT_SECRET);
-        res.header('Authorization', `Bearer ${token}`).send('Inicio de sesión exitoso.');
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error al iniciar sesión', error: err.message });
-      }
+  // Validation and sanitization
+  body("email").isEmail().withMessage("Email inválido").normalizeEmail(),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener al menos 6 caracteres")
+    .notEmpty()
+    .withMessage("La contraseña no debe estar vacía"),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    try {
+      const user = await User.findOne({
+        where: { email: req.body.email },
+        include: [
+          {
+            model: Role,
+            attributes: ["name"],
+          },
+        ],
+      });
+
+      if (!user) return res.status(400).send("Email o contraseña incorrectos.");
+
+      const validPass = await bcrypt.compare(req.body.password, user.password);
+
+      if (!validPass)
+        return res.status(400).send("Email o contraseña incorrectos.");
+
+      const token = jwt.sign(
+        { id: user.id, role: user.Role.name },
+        process.env.JWT_SECRET
+      );
+      res
+        .header("Authorization", `Bearer ${token}`)
+        .send("Inicio de sesión exitoso.");
+    } catch (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ message: "Error al iniciar sesión", error: err.message });
+    }
+  },
 ];
 
 //Método para obtener CRSF
 exports.csrfToken = (req, res) => {
-    const token = req.csrfToken();
-    res.json({ csrfToken: token });
+  const token = req.csrfToken();
+  res.json({ csrfToken: token });
 };
