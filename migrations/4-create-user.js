8202fbfcc4fedd9b240b1085286c4269393465ad
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-05 17:49:28 -0300

    .

--- migrations/4-create-user.js
+++ migrations/4-create-user.js
@@ -27,7 +27,7 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique:true,
+        unique: true,
       },
       phone: {
         type: Sequelize.STRING,
